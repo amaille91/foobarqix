f1d746772d70e62d0234bcb546ad66099c908377
Alexandre Maille <an email>
2022-02-06 14:31:33 +0100

    Implementation of the divisibleBy3 functionality

--- src/main/java/foobarqix/Main.java
+++ src/main/java/foobarqix/Main.java
@@ -1,9 +1,16 @@
 package foobarqix;
 
+import java.math.BigInteger;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+	}
+
+	public static String compute(String nbStr) {
+		BigInteger number = new BigInteger((nbStr));
+		return number.mod(new BigInteger("3")).equals(BigInteger.ZERO) ? "Fizz" : nbStr;
 	}
 
 }
---
+++ src/test/java/foobarqix/MainTest.java
@@ -0,0 +1,61 @@
+package foobarqix;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigInteger;
+import java.util.function.Predicate;
+import java.util.stream.IntStream;
+
+import org.assertj.core.api.Assertions;
+
+import net.jqwik.api.Arbitraries;
+import net.jqwik.api.Arbitrary;
+import net.jqwik.api.ForAll;
+import net.jqwik.api.Property;
+import net.jqwik.api.Provide;
+import net.jqwik.api.constraints.Positive;
+
+public class MainTest {
+
+	@Property
+	void any_multiple_of_3_should_start_with_Fizz(@ForAll @Positive BigInteger number) {
+		BigInteger multipleOf3 = number.multiply(new BigInteger("3"));
+
+		String result = Main.compute(multipleOf3.toString());
+
+		assertThat(result).startsWith("Fizz");
+	}
+
+	@Property
+	void non_numeric_inputs_should_result_in_NumberFormatException(
+			@ForAll("non numeric strings") String nonNumericStr) {
+
+		try {
+			Main.compute(nonNumericStr);
+			Assertions.fail("Expected NumberFormatException not thrown");
+		} catch (NumberFormatException e) {
+			// test ok
+		}
+
+	}
+
+	@Provide("non numeric strings")
+	Arbitrary<String> nonNumericStrings() {
+		return Arbitraries.strings().filter(Predicate.not(MainTest::isNumeric));
+	}
+
+	private static boolean isNumeric(String str) {
+		return str.startsWith("-") || str.startsWith("+")
+				? allNumericIntchars(str.substring(1).chars())
+				: allNumericIntchars(str.chars());
+	}
+
+	private static boolean allNumericIntchars(IntStream charStream) {
+		return charStream.filter(MainTest::isCharOtherThanDigit).findAny().isEmpty();
+	}
+
+	private static boolean isCharOtherThanDigit(int charInt) {
+		return !Character.isDigit((char) charInt);
+	}
+
+}
